2cdd4f6db56d97fcd24f8e91b890678ce519e72f
Lawrence <Lawrence.martin.green@gmail.com>
2022-03-20 22:39:31 +0000

    Win32: Fill window with colour to plan rect system

--- Source/Win32/Proc.c
+++ Source/Win32/Proc.c
@@ -13,10 +13,26 @@ LRESULT CALLBACK SolomonWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
             DwmExtendFrameIntoClientArea(hwnd, &m);
             break;
         }
+
         case WM_DESTROY:
             // Send the quit message to the operating system
             PostQuitMessage(0);
             return 0;
+
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            FillRect(hdc, &rect, CreateSolidBrush(RGB(40, 44, 52)));
+
+            SetTextColor(hdc, RGB(229, 129, 123));
+            SetBkColor(hdc, RGB(40, 44, 52));
+            TextOut(hdc, 4, 4, "Hello, Solomon!", 16);
+
+            EndPaint(hwnd, &ps);
+        }
     }
 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
