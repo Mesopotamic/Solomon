474f4c62e2e8d02a12cb2667a1eef93000a9647b
Lawrence <Lawrence.martin.green@gmail.com>
2022-03-21 23:20:47 +0000

    Add close event to example

--- Examples/HelloSolomon/Main.c
+++ Examples/HelloSolomon/Main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include "Solomon.h"
 
+/**
+ * @brief Declare a Solomon window handle to be shared throughout the program
+ */
+SolomonWindow window = NULL;
+
+/**
+ * @brief Declare a function to handle key inputs for us
+ * @param key The key code
+ * @param e event type
+ */
+void defaultSolomonKeyHandler(SolomonKey key, SolomonKeyEvent e);
+
+/**
+ * @brief Solomon Entry point. This is a macro that abstracts main, winmain etc, so that all Solomon apps can
+ * have one entry point and users don't have to handle that
+ */
 int SolomonMain(int argc, char* argv[])
 {
     // Show that we do in fact recieve command line args
@@ -10,12 +26,14 @@ int SolomonMain(int argc, char* argv[])
         printf("\t*%i: %s\n", i, argv[i]);
     }
 
-    // Create a Solomon window
-    SolomonWindow window = SolomonWindowCreate(0, 0, 720, 360, "Hello Solomon!");
+    // Create a Solomon window and attach our custom key command handler
+    // So that we can make solomon exit when the user presses esc
+    window = SolomonWindowCreate(0, 0, 720, 360, "Hello Solomon!");
     if (!window) {
         printf("\nWhoops, window creation failed!\n");
         return -1;
     }
+    SolomonKeyEventAttachHandler(window, defaultSolomonKeyHandler);
 
     // Now that the window has been created, we can show it to the user. Then we enter in a windowing loop.
     // Once per frame we have to evaluate all the events that happened, including if the OS requested the
@@ -24,5 +42,18 @@ int SolomonMain(int argc, char* argv[])
     while (SolomonWindowShouldContinue(window)) {
         SolomonWindowEvaluateEvents(window);
     }
+
+    // The windowing loop has finished so let's clean up solomon and inform the user that the exit has taken
+    // place
+    printf("Solomon ended\n");
     return 0;
+}
+
+void defaultSolomonKeyHandler(SolomonKey key, SolomonKeyEvent e)
+{
+    // Exit when the user presses esc key
+    if (key == SolomonKeyESC && e == SolomonKeyEventDown) {
+        SolomonWindowScheduleClose(window);
+        printf("Solomon Exit requested\n");
+    }
 }
