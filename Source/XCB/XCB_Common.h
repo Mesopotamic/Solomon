acd20e472c66c6878a817d09bcd134f3cc5eb2ed
Lawrence <Lawrence.martin.green@gmail.com>
2022-04-01 22:54:02 +0100

    XCB: Common header for XCB folder

---
+++ Source/XCB/XCB_Common.h
@@ -0,0 +1,16 @@
+#include <xcb/xcb.h>
+#include "SolomonCommon.h"
+
+typedef struct SolomonWindowXCB {
+    SolomonWindowCommon com;
+    xcb_window_t window_id;
+} SolomonWindowXCB;
+
+// Pointer to the global xcb connection for this application
+extern xcb_connection_t* s_connection;
+
+/**
+ * @brief Ensures that we have a connection to XCB, if we don't try to establish it, or return an error code
+ * @returns Solomon error code
+ */
+SolomonEnum connect_to_x();
